c60cbc4b6a7ddcd222029890025c995490f3a686
klimrus61 <klimrus61@mail.ru>
2022-11-20 12:21:50 +0300

    рефракторинг

--- parktronic/urls.py
+++ parktronic/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from web_app import views
 from web_app import urls as api_urls
 from rest_framework.authtoken.views import obtain_auth_token
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -10,4 +12,5 @@ urlpatterns = [
     path('', views.Index.as_view()),
     path('token/', obtain_auth_token),
     path('api/', include(api_urls, namespace='web_app')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
