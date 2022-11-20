bc3e40065670cb8bd5d3fb443a5ff73e50065275
klimrus61 <klimrus61@mail.ru>
2022-11-20 05:24:29 +0300

    установлены декораторы для персон детаил

--- web_app/views.py
+++ web_app/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.parsers import JSONParser
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 
 from django.http import HttpResponse, JsonResponse
 
@@ -121,6 +122,8 @@ def add_new_person(request):
             return JsonResponse(serializer.data)
         return JsonResponse(serializer.errors, status=400)
 
+@api_view(['GET'])
+@permission_classes([AllowAny,])
 def person_detail(request, pk):
     """
     Взаимодействие с конкретным пользователем
